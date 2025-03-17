415caeb2a7ea617e823cc2d6777016df51481ac8
Ken <ahmadsoud63@gmail.com>
2025-03-17 22:06:54 +0300

    add patron service and repo

--- demo/src/main/java/com/library/demo/Patron/Patron.java
+++ demo/src/main/java/com/library/demo/Patron/Patron.java
@@ -24,4 +24,29 @@ public class Patron {
         this.name = name;
         this.contactInformation = contactInformation;
     }
+
+    //setters and getters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
 }
---
+++ demo/src/main/java/com/library/demo/Patron/PatronService.java
@@ -0,0 +1,46 @@
+package com.library.demo.Patron;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PatronService {
+
+    @Autowired
+    private PatronRepository PatronRepository;
+
+    // GET all
+    public List<Patron> getAllPatrons() {
+        return PatronRepository.findAll();
+    }
+
+    // POST
+    public Patron savePatron(Patron Patron) {
+        return PatronRepository.save(Patron);
+    }
+
+    // GET
+    public Optional<Patron> getPatronById(Long id) {
+        return PatronRepository.findById(id);
+    }
+
+     // PUT
+     public Patron updatePatron(Long id, Patron PatronDetails) {
+        Patron existingPatron = PatronRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Patron not found with id: " + id));
+
+        existingPatron.setName(PatronDetails.getName());
+        existingPatron.setContactInformation(PatronDetails.getContactInformation());
+
+        return PatronRepository.save(existingPatron);
+    }
+
+    // DELETE
+    public void deletePatron(Long id) {
+        Patron existingPatron = PatronRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Patron not found with id: " + id));
+
+        PatronRepository.delete(existingPatron);
+    }
+}
